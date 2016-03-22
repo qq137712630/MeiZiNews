2d08e364d64dd0e884df4a4809ffbd7ccbeab4b7
jqc <1525626676@qq.com>
2016-03-22 22:31:28 +0800

    正在做开发周报: 开始做周报详情

---
+++ app/src/main/java/com/ms/meizinewsapplication/features/main/activity/DevWeekDetailActivity.java
@@ -0,0 +1,10 @@
+package com.ms.meizinewsapplication.features.main.activity;
+
+import com.ms.meizinewsapplication.features.base.activity.BaseActivityPresenterImpl;
+import com.ms.meizinewsapplication.features.main.iview.DevWeekDetailIVew;
+
+/**
+ * Created by 啟成 on 2016/3/22.
+ */
+public class DevWeekDetailActivity extends BaseActivityPresenterImpl<DevWeekDetailIVew> {
+}
--- app/src/main/java/com/ms/meizinewsapplication/features/main/iview/DevWeekDetailIVew.java
+++ app/src/main/java/com/ms/meizinewsapplication/features/main/iview/DevWeekDetailIVew.java
@@ -1,7 +1,15 @@
 package com.ms.meizinewsapplication.features.main.iview;
 
+import com.ms.meizinewsapplication.R;
+
+import org.loader.view.ViewImpl;
+
 /**
  * Created by 啟成 on 2016/3/22.
  */
-public class DevWeekDetailIVew {
+public class DevWeekDetailIVew extends ViewImpl {
+    @Override
+    public int getLayoutId() {
+        return R.layout.app_bar_dev_week_detail;
+    }
 }
