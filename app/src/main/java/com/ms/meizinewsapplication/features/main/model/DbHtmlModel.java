ca4ea3c311d69f8d43e1ee3cf18c9cbaa0cfc9b3
jqc <1525626676@qq.com>
2016-04-05 23:19:26 +0800

    正在做搜索界面： 开始加入html数据库：使用RxJava处理加入数据库

--- app/src/main/java/com/ms/meizinewsapplication/features/main/model/DbHtmlModel.java
+++ app/src/main/java/com/ms/meizinewsapplication/features/main/model/DbHtmlModel.java
@@ -4,6 +4,15 @@ import android.app.Activity;
 
 import com.ms.meizinewsapplication.features.base.model.DbModel;
 import com.ms.meizinewsapplication.utils.tool.ConstantData;
+import com.ms.meizinewsapplication.utils.tool.DebugUtil;
+import com.ms.retrofitlibrary.util.RxJavaUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.functions.Func1;
 
 /**
  * Created by 啟成 on 2016/4/5.
@@ -24,7 +33,45 @@ public class DbHtmlModel extends DbModel {
             String title,
             String html
     ) {
-        dbUtil.addHtml(url, ConstantData.DB_HTML_TYPE_ADD, title, html);
+        Map<String, String> map = new HashMap<>();
+        map.put("url", url);
+        map.put("title", title);
+        map.put("html", html);
+
+        Observable ob = Observable.just(map)
+                .map(new Func1<Map<String, String>, Boolean>() {
+                    @Override
+                    public Boolean call(Map<String, String> stringMap) {
+
+                        dbUtil.addHtml(
+                                stringMap.get("url"),
+                                ConstantData.DB_HTML_TYPE_ADD,
+                                stringMap.get("title"),
+                                stringMap.get("html")
+                        );
+                        return true;
+                    }
+                });
+        RxJavaUtil.rxIoAndMain(ob, new Subscriber<Boolean>() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+                DebugUtil.debugLogErr(e, "DbHtmlModel+++++\n" + e.toString());
+            }
+
+            @Override
+            public void onNext(Boolean aBoolean) {
+
+                DebugUtil.debugLogD("DbHtmlModel+++++\n" + aBoolean);
+            }
+
+        });
+
     }
 
 }
