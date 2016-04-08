cb6a848e6325e290243fe568e34d81b8e8be12b1
jqc <1525626676@qq.com>
2016-04-09 00:13:32 +0800

    正在做搜索： 开始做分词

--- app/src/main/java/com/ms/meizinewsapplication/features/base/base_web/PullWordApi.java
+++ app/src/main/java/com/ms/meizinewsapplication/features/base/base_web/PullWordApi.java
@@ -2,8 +2,8 @@ package com.ms.meizinewsapplication.features.base.base_web;
 
 import java.util.Map;
 
-import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.POST;
 import retrofit2.http.QueryMap;
 import rx.Observable;
 
@@ -12,12 +12,12 @@ import rx.Observable;
  */
 public interface PullWordApi {
     /**
-     * http://103.37.149.178:16888/pullword/get.php?source=北京大学&param1=0&param2=1
+     * http://103.37.149.178:16888/pullword/get.php?source=北京大学&param1=0&param2=0
      * @param cacheControl
      * @param pullwordMap
      * @return
      */
-    @GET("get.php?")
+    @POST("pullword/get.php?")
     Observable<String> RxPullWord(
             @Header("Cache-Control") String cacheControl,
             @QueryMap Map<String, String> pullwordMap
