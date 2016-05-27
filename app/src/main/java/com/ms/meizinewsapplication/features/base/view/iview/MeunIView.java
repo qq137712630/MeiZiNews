4538c93ba8c09b755941f4d2c25c1938798c5efb
jqc <1525626676@qq.com>
2016-05-27 22:34:44 +0800

    正在做夜间模式:开始做切换

--- app/src/main/java/com/ms/meizinewsapplication/features/base/view/iview/MeunIView.java
+++ app/src/main/java/com/ms/meizinewsapplication/features/base/view/iview/MeunIView.java
@@ -100,7 +100,9 @@ public class MeunIView extends ViewImpl {
         mColorful = new Colorful.Builder(appCompatActivity)
 
                 // 设置view的背景图片
+                .backgroundDrawable(R.id.lin_nav_header, R.attr.side_nav_bar)
                 .backgroundColor(R.id.nav_view, R.attr.root_view_bg)
+
                 .navigationViewItemColor(R.id.nav_view, R.attr.text_color)
                 .create();
         mColorful.setTheme(R.style.DayTheme_NoActionBar);
