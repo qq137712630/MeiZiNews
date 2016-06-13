fc4a17ca06828a338ab060981d189fc8be293261
jqc <1525626676@qq.com>
2016-06-13 22:55:24 +0800

    完成 视频列表 的夜间模式完成

--- app/src/main/java/com/ms/meizinewsapplication/features/video/iview/VideoItemIView.java
+++ app/src/main/java/com/ms/meizinewsapplication/features/video/iview/VideoItemIView.java
@@ -2,13 +2,13 @@ package com.ms.meizinewsapplication.features.video.iview;
 
 import android.app.Activity;
 
-import com.ms.meizinewsapplication.features.base.view.iview.ImgListIView;
+import com.ms.meizinewsapplication.features.base.view.iview.ColorfuImgListlViewImpl;
 import com.ms.meizinewsapplication.features.video.adapter.VideoAdapter;
 
 /**
  * Created by 啟成 on 2016/5/11.
  */
-public class VideoItemIView extends ImgListIView {
+public class VideoItemIView extends ColorfuImgListlViewImpl {
 
 
     @Override
