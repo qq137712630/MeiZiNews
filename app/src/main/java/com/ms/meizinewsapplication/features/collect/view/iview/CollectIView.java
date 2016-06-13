727b44bf84c9ad9fa268fc8f6f4664ff9439ff2a
jqc <1525626676@qq.com>
2016-06-13 23:18:18 +0800

    完成 收藏列表 的夜间模式完成

--- app/src/main/java/com/ms/meizinewsapplication/features/collect/view/iview/CollectIView.java
+++ app/src/main/java/com/ms/meizinewsapplication/features/collect/view/iview/CollectIView.java
@@ -1,5 +1,6 @@
 package com.ms.meizinewsapplication.features.collect.view.iview;
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
@@ -11,16 +12,19 @@ import android.view.View;
 
 import com.ms.greendaolibrary.db.HtmlEntity;
 import com.ms.meizinewsapplication.R;
+import com.ms.meizinewsapplication.features.base.utils.tool.ConstantData;
 import com.ms.meizinewsapplication.features.base.utils.tool.DebugUtil;
-import com.ms.meizinewsapplication.features.base.view.iview.RecyclerIView;
+import com.ms.meizinewsapplication.features.base.utils.tool.MyProfile;
+import com.ms.meizinewsapplication.features.base.view.iview.RxBusRecyclerIView;
 import com.ms.meizinewsapplication.features.collect.adapter.CollectAdapter;
+import com.ms.mythemelibrary.lib.Colorful;
 
 import java.util.List;
 
 /**
  * Created by 啟成 on 2016/4/19.
  */
-public class CollectIView extends RecyclerIView{
+public class CollectIView extends RxBusRecyclerIView {
     private CollectAdapter collectAdapter;
     private Toolbar toolbar;
 
@@ -46,8 +50,26 @@ public class CollectIView extends RecyclerIView{
     //TODO init========================================
 
     public void init(AppCompatActivity appCompatActivity) {
+
         initToolbar(appCompatActivity);
         initRecycler_list(appCompatActivity);
+        super.init(appCompatActivity);
+    }
+
+    @Override
+    public void initColorful(Activity activity) {
+
+        this.activity = activity;
+        initRecyclerViewSetter();
+        mColorful = new Colorful.Builder(activity)
+                .setter(recyclerViewSetter) // 手动设置setter
+                .backgroundColor(R.id.coordinator_layout, R.attr.root_view_bg)
+                .backgroundColor(R.id.toolbar, R.attr.colorPrimary)
+                .create();
+
+        isDay = MyProfile.getInstance(activity).getTheme().equals(ConstantData.MY_PROFILE_THEME_DAY);
+
+        setTheme(isDay);
     }
 
     //TODO View========================================
